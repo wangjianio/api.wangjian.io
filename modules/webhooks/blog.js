daf6b14cf9372ab67f0888591a2801f23a4a09f1
Wang Jian <337034664@qq.com>
2018-03-19 21:51:51 +0800

    更新

--- modules/webhooks/blog.js
+++ modules/webhooks/blog.js
@@ -2,10 +2,10 @@ const cp = require('child_process');
 const { log } = require('../../utils');
 
 function update(req, res) {
-  console.log(req.body);
+  console.log(req.headers);
 
   const output = cp.execSync('cd ../blog/; ls', { encoding: 'utf-8' });
-  log('webhooks', { output, req: req.body });
+  log('webhooks', { output, req: req.headers });
   res.send();
 }
 

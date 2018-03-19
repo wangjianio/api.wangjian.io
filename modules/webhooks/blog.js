306f9ce12daea9e16b5e53a4328f2a13c7118f8a
Wang Jian <337034664@qq.com>
2018-03-19 22:35:35 +0800

    webhook 完成

--- modules/webhooks/blog.js
+++ modules/webhooks/blog.js
@@ -6,12 +6,9 @@ function update(req, res) {
   const secret = fs.readFileSync('../github.secret', { encoding: 'utf-8' });
   const { s } = req.query;
 
-  console.log(s, secret);
-
   if (s === secret) {
-    const output = cp.execSync('cd ../blog/; ls', { encoding: 'utf-8' });
+    const output = cp.execSync('cd ../blog/; git pull', { encoding: 'utf-8' });
     log('webhooks', { output });
-    console.log('success');
   }
 
   res.send();

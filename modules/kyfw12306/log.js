7441958f33d648a9f37790d8238ea5e207ef6058
Wang Jian <337034664@qq.com>
2018-07-03 23:09:57 +0800

    自动更新 station_name

--- modules/kyfw12306/log.js
+++ modules/kyfw12306/log.js
@@ -1,7 +1,14 @@
 const { log } = require('../../utils');
+const update = require('./updateStationNameFile');
 
 module.exports = (req = {}, obj = {}) => {
   const { url, method, query, params, headers, route } = req;
+
+  // 周日上午更新
+  if (new Date().getDay() === 0 && new Date().getHours() < 12) {
+    update();
+  }
+
 
   const newObj = {
     ...obj,
---
+++ modules/kyfw12306/updateStationNameFile.js
@@ -0,0 +1,58 @@
+const https = require('https');
+const fs = require('fs');
+const cp = require('child_process');
+const sendMail = require('../mail/send');
+
+module.exports = function () {
+  const localVersion = fs.readFileSync('./modules/kyfw12306/lib/local_version').toString();
+
+  const options = {
+    hostname: 'kyfw.12306.cn',
+    path: '/otn/leftTicket/init',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+    }
+  };
+
+  https.get(options, res => {
+    if (res.statusCode === 200) {
+      let html = '';
+      res.on('data', chunk => {
+        html += `${chunk}`;
+      });
+
+
+      res.on('end', () => {
+        // 匹配版本号
+        const result = html.match(/station_name\.js\?station_version=(.*?)['"]/);
+
+        if (result.length >= 2) {
+          const latestVersion = result[1];
+
+          // 版本号不同，下载文件
+          if (localVersion !== latestVersion) {
+            console.log('Have update')
+
+            /**
+             * 下载文件
+             * TODO: 下载失败不执行下一步
+             */
+            cp.execSync(`cd modules/kyfw12306/lib; wget -a wget.log -b -O station_name.js https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=${latestVersion}`, { encoding: 'utf-8' });
+
+            // 修改本地存储版本号
+            fs.writeFile('./modules/kyfw12306/lib/local_version', latestVersion, () => { });
+
+            // 发送邮件提醒
+            sendMail('【api.wangjian.io】station_name.js 更新完成', `更新前版本号：${localVersion}\n当前版本号：${latestVersion}`)
+          } else {
+            console.log('Updated')
+          }
+        }
+      })
+    }
+  });
+}
+
+
+
+

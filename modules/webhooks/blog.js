987d4e18b641e07b67b7449dd2109ff232129f17
Wang Jian <337034664@qq.com>
2018-08-30 21:46:47 +0800

    debug

--- modules/webhooks/blog.js
+++ modules/webhooks/blog.js
@@ -3,10 +3,15 @@ const cp = require('child_process');
 const { log } = require('../../utils');
 
 function update(req, res) {
+  console.log('update');
   const { s } = req.query;
-
+  console.log('s:', s);
+  console.log('config:', config.github.secret);
+  
   if (s === config.github.secret) {
+    console.log('===')
     const output = cp.execSync('cd ../blog/; git pull', { encoding: 'utf-8' });
+    console.log(output)
     log('webhooks', { output });
   }
 

a7bafcadf44e77939056ce77ccac787708a70f05
Wang Jian <337034664@qq.com>
2018-08-30 22:10:47 +0800

    更新 blog webhook

--- modules/webhooks/blog.js
+++ modules/webhooks/blog.js
@@ -3,19 +3,23 @@ const cp = require('child_process');
 const { log } = require('../../utils');
 
 function update(req, res) {
-  console.log('update');
+  const { route } = req;
   const { s } = req.query;
-  console.log('s:', s);
-  console.log('config:', config.github.secret);
-  
-  if (s === config.github.secret) {
-    console.log('===')
-    const output = cp.execSync('cd ../blog/; git pull', { encoding: 'utf-8' });
-    console.log(output)
-    log('webhooks', { output });
-  }
 
-  res.send();
+  if (s === config.github.secret) {
+    const output = cp.execSync('cd ../blog/; git pull', { encoding: 'utf-8' });
+    log('webhooks', { path: route.path, output });
+
+    res.send(JSON.stringify({
+      statusCode: 0,
+      message: 'success',
+    }));
+  } else {
+    res.send(JSON.stringify({
+      statusCode: 40,
+      message: 'invalid secret',
+    }));
+  }
 }
 
 module.exports = {

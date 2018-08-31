fde30af14d047ca5d2e6b20486b869a48015e799
Wang Jian <337034664@qq.com>
2018-08-31 15:13:35 +0800

    增加 mp2 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [{
+    name: 'api',
+    script: 'index.js',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+    watch: true,
+    watch_options: {
+      // awaitWriteFinish: true,
+    },
+    ignore_watch: [
+      'node_modules/',
+      'logs/',
+    ],
+  }],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};

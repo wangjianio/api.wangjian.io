889c8aa531f7853002289c699e7c9cd394aa8db6
Wang Jian <337034664@qq.com>
2018-08-31 18:07:50 +0800

    测试代码

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,8 +26,8 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/wangjianio/api.wangjian.io.git',
       path: '/etc/node/test',
-      "post-setup": "mkdir psetup",
-      'post-deploy': 'mkdir pdeploy'
+      "post-setup": "mkdir psetup; pwd > s",
+      'post-deploy': 'mkdir pdeploy; pwd > d'
     }
   }
 };

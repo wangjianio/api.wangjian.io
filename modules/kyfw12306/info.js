f3d17d71df4ec944abb26a7cf53efde8aa47d53c
Wang Jian <337034664@qq.com>
2018-09-17 00:34:52 +0800

    更新 workflow 版本信息

--- modules/kyfw12306/info.js
+++ modules/kyfw12306/info.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
   const info = {
-    latest_version: "2.3",
-    url: "https://workflow.is/workflows/9854ff939a9442879d7430d773ebe3c0",
-    update: "2018-07-03"
+    latest_version: "2.4",
+    url: "https://workflow.is/workflows/355dfce33b5f4876a709b1990e0e2661",
+    update: "2018-09-17"
   }
 
   res.send(JSON.stringify(info));

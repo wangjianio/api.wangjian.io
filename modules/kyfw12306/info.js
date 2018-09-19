6966c44408d8f2027164cda6571298092c9fe312
Wang Jian <337034664@qq.com>
2018-09-19 18:52:08 +0800

    更新捷径版本

--- modules/kyfw12306/info.js
+++ modules/kyfw12306/info.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const info = {
-    latest_version: "2.5",
-    url: "https://www.icloud.com/shortcuts/b9001e8898a34b5f86d0128eabf1602a",
+    latest_version: "2.6",
+    url: "https://www.icloud.com/shortcuts/a91b6cb2f5d5482c9776dbeb41ab12dc",
     update: "2018-09-19"
   }
 

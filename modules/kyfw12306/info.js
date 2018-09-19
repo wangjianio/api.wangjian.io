f8a62f4cdf92b36a253bdf0ef48c80be5213f224
Wang Jian <337034664@qq.com>
2018-09-19 16:06:18 +0800

    更新捷径版本信息

--- modules/kyfw12306/info.js
+++ modules/kyfw12306/info.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
   const info = {
-    latest_version: "2.4",
-    url: "https://workflow.is/workflows/355dfce33b5f4876a709b1990e0e2661",
-    update: "2018-09-17"
+    latest_version: "2.5",
+    url: "https://www.icloud.com/shortcuts/b9001e8898a34b5f86d0128eabf1602a",
+    update: "2018-09-19"
   }
 
   res.send(JSON.stringify(info));

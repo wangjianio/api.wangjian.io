5c9d837a0aa0797a2a76c4abe9a9d6958381c254
Wang Jian <337034664@qq.com>
2018-10-31 20:48:49 +0800

    简单更新

--- modules/kyfw12306/lib/getTrainNo3.js
+++ modules/kyfw12306/lib/getTrainNo3.js
@@ -1,19 +1,34 @@
 const trainList = require('./train_list.json');
+const getTrainNo1 = require('./getTrainNo1');
+const getTrainNo2 = require('./getTrainNo2');
 
-module.exports = function getTrainNo({ date, trainCode } = {}) {
+module.exports = function getTrainNo({ date, trainCode, from } = {}) {
   return new Promise((resolve, reject) => {
     try {
       let key = 'O';
       if (['C', 'D', 'G', 'K', 'T', 'Z'].includes(trainCode[0])) {
         key = trainCode[0];
       }
-  
-      const trainNo = trainList[date][key].find(item => {
+
+      const matched = trainList[date][key].find(item => {
         const pattern = new RegExp(`${trainCode}\\(`);
         return pattern.test(item.station_train_code);
-      }).train_no;
+      });
 
-      resolve(trainNo);
+      if (matched) {
+        resolve(matched.train_no);
+      } else {
+        // getTrainNo1({ date, trainCode, from }).then(trainNo => {
+        //   resolve(trainNo);
+        // }, err => {
+          // getTrainNo2({ date, trainCode, from }).then(trainNo => {
+          //   resolve(trainNo);
+          // }, err => {
+          //   reject('error: getTrainNo');
+          // })
+        // })
+      }
+
     } catch (error) {
       reject('error: getTrainNo');
       console.log(error);

d2be077e0ecdf0b6132731190362a4d0d3b31e9f
Wang Jian <337034664@qq.com>
2018-12-27 15:39:23 +0800

    增加cookie

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const axios = require('axios');
 global.axios = axios;
 
 axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+axios.defaults.headers.common['Cookie'] = 'RAIL_EXPIRATION=1546129402314; RAIL_DEVICEID=Otw0i6GPbYUyVA9oXIBMhSsnnvrQF82_X2Toqo9vbLvhsITs2R23P44IyKWSz_DV7jwh3KwBzSh_AEuCvLy2TiT2ybytPobJPbwoH4wzcSZBrx1geT0jx1kCKrSr4K9i-bVFjW0b1HX4sIDnF_cFnsCuDtcP_Gai; BIGipServerpool_restapi=2262893066.44838.0000';
 
 
 

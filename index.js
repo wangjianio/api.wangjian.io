74d04239912ffa2f7d76c7028e83618a73ebc66d
Wang Jian <337034664@qq.com>
2019-01-28 17:43:38 +0800

    删除 cookie

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const axios = require('axios');
 
 global.axios = axios;
 
-axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36';
 // TODO: 动态获取 cookie
-axios.defaults.headers.common['Cookie'] = 'RAIL_EXPIRATION=1546230124604; RAIL_DEVICEID=bv1CvurlKB480zbJghKl87TZWFjvG-eM-0L6dsD46N7nn7DMlPOwc4xyHpNpI9ZNZgRY7wj3pHZLt5UG5KmlqkVc-40-ohEw00XHott6Oi0SfmeYXbjkVbqg6ftna9914vUzS1EgtyCm7n7asvjWhDKGvRpKtZn1; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=1206911498.38945.0000; BIGipServerpool_index=787481098.43286.0000';
+// axios.defaults.headers.common['Cookie'] = 'RAIL_EXPIRATION=1546230124604; RAIL_DEVICEID=bv1CvurlKB480zbJghKl87TZWFjvG-eM-0L6dsD46N7nn7DMlPOwc4xyHpNpI9ZNZgRY7wj3pHZLt5UG5KmlqkVc-40-ohEw00XHott6Oi0SfmeYXbjkVbqg6ftna9914vUzS1EgtyCm7n7asvjWhDKGvRpKtZn1; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=1206911498.38945.0000; BIGipServerpool_index=787481098.43286.0000';
 
 
 

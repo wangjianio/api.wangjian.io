41924976820dfb971a2960ebc51c981aea899dae
王建 <wangjianio@users.noreply.github.com>
2020-08-04 13:50:53 +0800

    更新快捷指令版本

--- modules/kyfw12306/info.js
+++ modules/kyfw12306/info.js
@@ -5,9 +5,9 @@ const mongo = require('../../utils/mongo');
 
 module.exports = (request, response) => {
   const info = {
-    latest_version: "3.1",
-    url: "https://www.icloud.com/shortcuts/a4626693baca46e786095ecfe4f001ab",
-    update: "2018-11-19"
+    latest_version: "3.2",
+    url: "https://www.icloud.com/shortcuts/2edc41bfe0784a84be23aab4dc747ed8",
+    update: "2020-08-04"
   }
 
   response.send(JSON.stringify(info));
